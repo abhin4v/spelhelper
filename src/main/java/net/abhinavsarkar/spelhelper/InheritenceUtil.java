4534aa8eb0e68cdaf7bef49431ed7d6a3d3b1810
Abhinav Sarkar <abhinav.sarkar@gmail.com>
2010-05-27 20:24:57 +0530

    Added license. Added more info in pom.

--- src/main/java/net/abhinavsarkar/spelhelper/InheritenceUtil.java
+++ src/main/java/net/abhinavsarkar/spelhelper/InheritenceUtil.java
@@ -1,3 +1,20 @@
+/* Copyright 2010 Abhinav Sarkar <abhinav@abhinavsarkar.net>
+ *
+ * This file is a part of SpelHelper library.
+ *
+ * SpelHelper library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (GNU LGPL) as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * SpelHelper library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with SpelHelper library.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.abhinavsarkar.spelhelper;
 
 import java.util.LinkedHashSet;
@@ -14,7 +31,7 @@ final class InheritenceUtil {
 
     /**
      * Get inheritance of type.
-     * 
+     *
      * @param in
      * @param result
      */
@@ -31,7 +48,7 @@ final class InheritenceUtil {
 
     /**
      * Get interfaces that the type inherits from.
-     * 
+     *
      * @param in
      * @param result
      */
@@ -45,7 +62,7 @@ final class InheritenceUtil {
 
     /**
      * Get superclass of class.
-     * 
+     *
      * @param in
      * @return
      */
--- src/main/java/net/abhinavsarkar/spelhelper/SpelHelper.java
+++ src/main/java/net/abhinavsarkar/spelhelper/SpelHelper.java
@@ -1,3 +1,20 @@
+/* Copyright 2010 Abhinav Sarkar <abhinav@abhinavsarkar.net>
+ *
+ * This file is a part of SpelHelper library.
+ *
+ * SpelHelper library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (GNU LGPL) as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * SpelHelper library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with SpelHelper library.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.abhinavsarkar.spelhelper;
 
 import static java.util.Arrays.asList;
